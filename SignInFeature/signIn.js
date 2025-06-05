f1312b91c26b4c7b447169fbaf01e1b1dc649908
StHuyse <ffgshj321@gmail.com>
2025-06-05 15:13:13 +0700

    Update Sign in feature

---
+++ SignInFeature/signIn.js
@@ -0,0 +1,42 @@
+const { getDriver } = require('./driver');
+
+async function typeTextWithKeys(driver, text) {
+  const actions = text.split('').map(char => ({ type: 'keyDown', value: char }));
+
+  await driver.performActions([{ type: 'key', id: 'keyboard', actions }]);
+  await driver.releaseActions();
+}
+
+async function signInFunction() {
+  const driver = await getDriver();
+
+  try {
+    try {
+      const permissionButton = await driver.$('//*[@text="Cho phép"]');
+      if (await permissionButton.isDisplayed()) {
+        await permissionButton.click();
+      }
+    } catch (_) {}
+
+    const usernameField = await driver.$('~TAG_USERNAME');
+    await usernameField.click();
+    await typeTextWithKeys(driver, 'khua01@gmail.com');
+
+    const passwordField = await driver.$('~TAG_PASSWORD');
+    await passwordField.click();
+    await typeTextWithKeys(driver, '123456');
+    await driver.hideKeyboard();
+
+    const rememberPassword = await driver.$('~TAG_REMEMBERPASSWORD');
+    await rememberPassword.click();
+
+    const signin = await driver.$('~TAG_BUTTON_SIGNIN');
+    await signin.click();
+
+    await driver.pause(2000);
+  } catch (error) {
+    console.error('Error in signInFunction:', error);
+  }
+}
+
+module.exports = { signInFunction };
